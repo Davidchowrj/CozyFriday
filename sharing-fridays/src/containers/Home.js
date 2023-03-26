62fb11e3fb3bea60c2fa13913d81b69d1add9add
David Chow <davidchowrj@gmail.com>
2023-03-27 00:14:57 +0800

    add readme

--- sharing-fridays/src/containers/Home.js
+++ sharing-fridays/src/containers/Home.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../logo.svg";
 
-const Home = () => (
-  <div className="container">
-    <div>Hello World</div>
-  </div>
-);
+const Home = () => {
+  const [counter, setCounter] = useState(null);
+
+  const handleCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  return (
+    <div className="container">
+      <div>
+        You've clicked {counter} times <br />
+        <button onClick={handleCounter}> Click Me</button>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
