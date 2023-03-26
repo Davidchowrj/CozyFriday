a376767d7861a0973781418afc68b8176b6e1e81
David Chow <davidchowrj@gmail.com>
2023-03-26 23:55:07 +0800

    init commit

---
+++ SharingFridays.md
@@ -0,0 +1,8 @@
+# React Dev Tools
+
+## What is it
+
+Dev tool that allows for:
+
+- debugging
+- tracking state
---
+++ sharing-fridays/src/containers/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "../logo.svg";
+
+const Home = () => (
+  <div className="container">
+    <div>Hello World</div>
+  </div>
+);
+
+export default Home;
